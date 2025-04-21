365565ef5c45dfb5a3dd4b48e7f190aa34a06ac5
suraj kumar <sooraj4475@gmail.com>
2025-04-21 16:53:19 +0530

    Added new projects

--- src/resources/Experiences.js
+++ src/resources/Experiences.js
@@ -15,34 +15,27 @@ export const Experiences = [
   },
   {
     _id: "002",
-    company: "Techweek Infotech LLC",
+    company: "SCS Globals Web & Soft Solutions ",
     title: "Front-end Developer_Intern",
-    period: "January 2024 - June 2024",
+    period: "July 2024 - December 2024",
     description: `
-    // Developed front-end for SkyDreamGroup (React.js)
-    function buildFrontend() {
-      const engagementBoost = "25%";
-      enhanceUIUX();    // Optimized user engagement
-      optimizeResponsiveDesign(); // Improved responsiveness by 30%
-    }
-
-    // Established efficient email handling
-    function setupEmailReceiver() {
-      use(NodeMailer);
-      reduceEmailHandlingTime(20%);
-    }
+    Built the front end of the Sky Dream Group website, a dynamic platform focused on well-being, using React.js. 
+    Boosted user engagement by 25percent through optimized UI/UX design and interactive interfaces.
+    Improved mobile responsiveness by 30percent by utilizing React.js to create responsive and interactive user 
+    interfaces across various devices. 
     `,
   },
   {
     _id: "003",
-    company: "Freelancing",
+    company: "SCS Globals Web & Soft Solutions ",
     title: "Front-end Developer",
-    period: "Present",
+    period: "January 2025 - Present",
     description: `
-    // Future projects and innovations coming soon...
-    function futureScope() {
-      return "Innovative solutions on the way!";
-    }
+    Developed and optimized 20+ React.js components for dynamic web applications. 
+    Implemented RESTful API integration for seamless data exchange, improving data retrieval speed by 40%.
+    Enhanced frontend performance by reducing rendering time by 25% and improving page load speed by 35%.
+    Collaborated with backend developers to enhance UI responsiveness and accessibility, increasing overall UX 
+    and usersatisfaction by 30%. 
     `,
   },
 ];
